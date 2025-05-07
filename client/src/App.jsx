8a8391b7752f77c85196597a955cd846dcfcf7f3
MaksymKonoplinskyi <gran7777777@gmail.com>
2025-05-07 11:28:30 +0300

    main change

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import EventSourcing from "./EventSoursing"
+import LongPulling from "./LongPulling"
+
+function App() {
+  return (
+    <div>
+      {/* <LongPulling /> */}
+      <EventSourcing />
+    </div>
+  )
+}
+
+export default App
